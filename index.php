165e40af38d67dd5b30af232dc2e5786ef57e1d2
bezlash <bezlash@gmail.com>
2014-11-18 13:54:33 +0000

    Initial commit.  Includes the bare PHP template of index.php, _header, _nav, _footer and an empty variables.php page

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+	<meta name="description" content="Iranian and persian food recipes" />
+
+	<meta name="keywords" content="Iranian, Persian, food, recipes" />
+
+	<meta charset="utf-8">
+
+	<title>Iranian and persian food recipes</title>
+
+	<?php include('variables/variables.php'); ?>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+	<?php include('includes/_header.php'); ?>
+	
+	<?php include('includes/_nav.php'); ?>
+		
+	<main>
+
+	<p>Main paragraph test</p>
+	
+	</main>
+	
+	<?php include('includes/_footer.php'); ?>
+	
+
+</div><!--end wrapper-->
+
+</body>
+
+</html>
