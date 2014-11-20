cc0b3962f81b79172652f9c07938efb7a86540b8
bezlash <bezlash@gmail.com>
2014-11-20 17:23:21 +0000

    Created aside sidebar and section with empty articles

--- index.php
+++ index.php
@@ -5,6 +5,33 @@
 	<?php include('includes/_nav.php'); ?>
 		
 	<main>
+	
+		<aside><!--sidebar-->
+
+			<h2>Dish Types</h2>
+
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+
+		</aside><!--end sidebar-->
+
+		<section>
+	
+			<h2>Latest Dishes</h2>
+
+				<article>
+
+				</article>
+
+				<article>
+
+				</article>
+
+		</section>
 
 	<p>Main paragraph test</p>
 	
