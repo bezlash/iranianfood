670ba3e74696498240e710c071fd16383311e171
bezlash <bezlash@gmail.com>
2014-11-25 16:00:14 +0000

    Turned my .scss into _partials; Added footer sections; completed the _typography

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,5 +1,12 @@
 <footer>
-	<p><?php echo $footer ?></p>
+
+	<section class="main_footer">
+		<p><?php echo $footer ?></p>
+	</section>
+
+	<section class="contact_details">
+		<p>Contact details</p>
+	</section>
 	
 </footer>
 
